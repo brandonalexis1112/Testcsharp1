48e27a7878b6d7bedb116d283216387d50a53cd8
BrandonAlexis <brandonalexis1112@gmail.com>
2021-08-12 02:41:41 -0500

    segundo commit roles

---
+++ TestCsharp/Controllers/ErrorController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestCsharp.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult UnauthorizedOperation(String operacion, String modulo, String msjeErrorException)
+        {
+            ViewBag.operacion = operacion;
+            ViewBag.modulo = modulo;
+            ViewBag.msjeErrorException = msjeErrorException;
+            return View();
+        }
+    }
+}
---
+++ TestCsharp/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestCsharp.Filters;
+
+namespace TestCsharp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [AuthorizeUser(idOperacion:1)]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AuthorizeUser(idOperacion: 3)]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        [AuthorizeUser(idOperacion: 4)]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
